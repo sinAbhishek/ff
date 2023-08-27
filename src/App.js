0cc1f1a7858eafb85cd62554608fd6bc169c8a04
sinAbhishek <singhabhshek979@gmail.com>
2023-08-27 15:28:06 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./components/card/Card";
+import { ClimbingBoxLoader } from "react-spinners/";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 function App() {
   const [data, setdata] = useState([]);
+  const [loader, setloader] = useState(true);
+  const [page, setpage] = useState(JSON.parse(localStorage.getItem("page")));
+  const [pageNo, setpageNo] = useState(1);
+  const increase = () => {
+    setloader(true);
+    setdata([]);
+    setpage(page + 1);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+  useEffect(() => {
+    localStorage.setItem("page", JSON.stringify(page));
+    setpageNo(JSON.parse(localStorage.getItem("page")));
+  }, [page]);
+
+  const decrease = () => {
+    setloader(true);
+    setdata([]);
+    page != 1 && setpage(page - 1);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
   useEffect(() => {
     const call = async () => {
-      const res = await axios.get("https://swapi.dev/api/people/?page=3");
+      const res = await axios.get(
+        `https://swapi.dev/api/people/?page=${pageNo}`
+      );
       console.log(res.data.results);
       setdata(res.data.results);
+      setloader(false);
     };
-    call();
-  }, []);
+    pageNo && call();
+  }, [pageNo]);
   return (
-    <div className="App flex flex-wrap justify-center">
-    
-      {data && data.map((c) => <Card chardetails={c} />)}
-    </div>
+    <>
+      <div className="App flex flex-wrap justify-center relative">
+        {data && data.map((c) => <Card chardetails={c} />)}
+        {data[0] && (
+          <div className="pageswitch absolute bottom-4 ">
+            <ArrowCircleLeftIcon
+              disabled={true}
+              onClick={() => decrease()}
+              className={page === 1 ? "left-btn disable" : "left-btn"}
+            />
+            <ArrowCircleRightIcon
+              onClick={() => increase()}
+              className="right-btn"
+            />
+          </div>
+        )}
+      </div>
+      {loader && (
+        <div className=" flex justify-center items-center w-full h-screen">
+          <ClimbingBoxLoader color="#f72d77" />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,35 +1,154 @@
 import React, { useEffect, useState } from "react";
 import ReactCardFlip from "react-card-flip";
-import "./card.css"
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import "./card.css";
+import axios from "axios";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const Card = ({ chardetails }) => {
   const [flipped, setflipped] = useState(false);
-  const [random,setrandom]=useState(1)
+  const [random, setrandom] = useState(1);
+  const [home, sethome] = useState();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   useEffect(() => {
     console.log(chardetails);
-    setrandom(Math.floor(Math.random()*2+1))
-  });
+    setrandom(Math.floor(Math.random() * 2 + 1));
+  }, []);
+  useEffect(() => {
+    const call = async () => {
+      const res = await axios.get(chardetails.homeworld);
+      console.log(res.data);
+      sethome(res.data);
+    };
+    chardetails && call();
+  }, [chardetails]);
   const flip = () => {
     setflipped(!flipped);
   };
   return (
     <div className="container rounded-lg">
-<div className="card rounded-lg ">
-  <div className="front rounded-lg">
-    <div className=" w-full h-1/2">
-    <img className="imstyle w-full h-full bg-contain " src={`./${chardetails.name}-${random}.jpeg`} alt="" />
-    </div>
-    <h1 className=" text-white font-bold my-4 mx-2 text-lg">{chardetails.name}</h1>
-    
-  </div>
-  <div className="back rounded-lg ">
-    <h1 className=" text-white font-semibold p-4 mt-8 text-xl ">Skin color : <span className=" italic">{chardetails.skin_color}</span></h1>
-    <h1 className=" text-white font-semibold p-4 text-xl">Hair color : <span className=" italic">{chardetails.hair_color}</span></h1>
-    <h1 className=" text-white font-semibold p-4 text-xl">Eye color : <span className=" italic">{chardetails.eye_color}</span></h1>
-    <h1 className=" text-white font-semibold p-4 text-xl">Gender : <span className=" italic">{chardetails.gender}</span></h1>
-  </div>
-</div>
-    </div>
+      <div
+        onClick={handleOpen}
+        className="card rounded-lg hover:cursor-pointer "
+      >
+        <div className="front rounded-lg">
+          <div className=" w-full h-1/2">
+            <img
+              className="imstyle w-full h-full bg-contain "
+              src={`./${chardetails.name}-${random}.jpeg`}
+              alt=""
+            />
+          </div>
+          <h1 className=" text-white font-bold my-4 mx-2 text-lg">
+            {chardetails.name}
+          </h1>
+        </div>
+        <div className="back rounded-lg ">
+          <h1 className=" text-white font-semibold p-4 mt-8 text-xl ">
+            Skin color :{" "}
+            <span className=" italic">{chardetails.skin_color}</span>
+          </h1>
+          <h1 className=" text-white font-semibold p-4 text-xl">
+            Hair color :{" "}
+            <span className=" italic">{chardetails.hair_color}</span>
+          </h1>
+          <h1 className=" text-white font-semibold p-4 text-xl">
+            Eye color : <span className=" italic">{chardetails.eye_color}</span>
+          </h1>
+          <h1 className=" text-white font-semibold p-4 text-xl">
+            Gender : <span className=" italic">{chardetails.gender}</span>
+          </h1>
+        </div>
+      </div>
 
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className=" relative flex flex-col justify-between h-full">
+            <h1 className=" text-4xl font-bold text-slate-100 ml-2">
+              {chardetails.name}
+            </h1>
+            <div className=" p-4 flex">
+              <img
+                className=" modimg"
+                src={`./${chardetails.name}-${random}.jpeg`}
+                alt=""
+              />
+              <div className=" ml-4">
+                <h1 className=" text-white">
+                  Height -{" "}
+                  <span className=" font-bold ">{chardetails.height}</span>
+                </h1>
+                <h1 className=" text-white">
+                  Mass - <span className=" font-bold ">{chardetails.mass}</span>
+                </h1>
+                <h1 className=" text-white">
+                  DateAdded -{" "}
+                  <span className=" font-bold ">
+                    {chardetails.created.slice(0, 10)}
+                  </span>
+                </h1>
+                <h1 className=" text-white">
+                  Number of films -{" "}
+                  <span className=" font-bold ">
+                    {chardetails.films.length}
+                  </span>
+                </h1>
+                <h1 className=" text-white">
+                  Birth Year -{" "}
+                  <span className=" font-bold ">{chardetails.birth_year}</span>
+                </h1>
+              </div>
+            </div>
+            {home && (
+              <div className="  w-full bg-slate-300">
+                <div className=" ml-4">
+                  <h1 className="  font-bold text-2xl text-gray-900 underline ">
+                    Homeworld
+                  </h1>
+                  <h1 className=" text-black font-semibold">
+                    Name - <span className=" font-bold">{home.name}</span>
+                  </h1>
+                  <h1 className=" text-black font-semibold">
+                    Terrain - <span className=" font-bold">{home.terrain}</span>
+                  </h1>
+                  <h1 className=" text-black font-semibold">
+                    Climate - <span className=" font-bold">{home.climate}</span>
+                  </h1>
+                  <h1 className=" text-black font-semibold">
+                    Residents -{" "}
+                    <span className=" font-bold">{home.residents.length}</span>
+                  </h1>
+                  <h1 className=" text-black font-semibold">
+                    Orbital period -{" "}
+                    <span className=" font-bold">{home.orbital_period}</span>
+                  </h1>
+                </div>
+              </div>
+            )}
+          </div>
+        </Box>
+      </Modal>
+    </div>
   );
 };
 
